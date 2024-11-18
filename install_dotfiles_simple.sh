c3d851647ca01d301d641558dc36107a7b68406a
Andrew Knoesen <andrew@spatialedge.co.za>
2024-11-18 15:27:29 +0200

    Made a simple version

--- install_dotfiles_simple.sh
+++ install_dotfiles_simple.sh
@@ -6,7 +6,7 @@ DOTFILES_DIR="$HOME/.dotfiles"
 # Create symlinks
 create_symlinks() {
     echo "Creating symlinks..."
-    ln -sf $DOTFILES_DIR/.zshrc $HOME/.zshrc
+    ln -sf $DOTFILES_DIR/.zshrc_simple $HOME/.zshrc
     ln -sf $DOTFILES_DIR/.gitconfig $HOME/.gitconfig
     ln -sf $DOTFILES_DIR/.p10k.zsh $HOME/.p10k.zsh
     # Add more symlinks as needed
@@ -44,50 +44,13 @@ install_oh_my_zsh_plugins() {
     # Add more plugin installations as needed
 }
 
-# Install additional tools
-install_additional_tools() {
-    echo "Installing additional tools..."
-    
-    # Install fzf
-    git clone --depth 1 https://github.com/junegunn/fzf.git $HOME/.fzf
-    source $HOME/.fzf/install --all
-    
-    # Install fzf-git.sh
-    git clone https://github.com/junegunn/fzf-git.sh.git $HOME/fzf-git.sh
-    
-    # Install thefuck
-    pip install thefuck
-    
-    # Install zoxide
-    curl -sS https://webinstall.dev/zoxide | bash
-}
-
-# Configure additional tools
-configure_additional_tools() {
-    echo "Configuring additional tools..."
-    
-    # Configure fzf
-    echo '[ -f ~/.fzf.zsh ] && source ~/.fzf.zsh' >> $HOME/.zshrc
-    
-    # Configure fzf-git.sh
-    echo 'source ~/fzf-git.sh/fzf-git.sh' >> $HOME/.zshrc
-    
-    # Configure thefuck
-    echo 'eval $(thefuck --alias)' >> $HOME/.zshrc
-    
-    # Configure zoxide
-    echo 'eval "$(zoxide init zsh)"' >> $HOME/.zshrc
-}
-
 # Main installation process
 main() {
     install_zsh
     install_oh_my_zsh
     install_powerlevel10k
     install_oh_my_zsh_plugins
-    install_additional_tools
     create_symlinks
-    # configure_additional_tools
 
     echo "Dotfiles installation complete!"
     echo "Please restart your shell or run 'source ~/.zshrc' to apply changes."
